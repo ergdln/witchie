2b40898662b91c898afbd7d5a0faee3413c122ad
egln <egln@10-48-48-155.0200.ufpe.br.iosda.org>
2023-07-20 14:45:52 -0300

    initial commit

---
+++ witchie/witchie/witchieApp.swift
@@ -0,0 +1,17 @@
+//
+//  witchieApp.swift
+//  witchie
+//
+//  Created by egln on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct witchieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
