36eab96bec57e4e0f30065a1b0968679c42cc111
ditthales <tvgf@academy.cin.ufpe.br>
2023-07-26 16:39:27 -0300

    adding-level-completed-y/n-and-blocking-levels

---
+++ witchie/witchie/LevelCompleted.swift
@@ -0,0 +1,12 @@
+//
+//  LevelCompleted.swift
+//  witchie
+//
+//  Created by ditthales on 26/07/23.
+//
+
+import Foundation
+
+struct LevelCompleted{
+    static var isCompleted: [Bool] = Array(repeating: false, count: LevelModel.patchOne().count)
+}
--- witchie/witchie/LevelSelectorView.swift
+++ witchie/witchie/LevelSelectorView.swift
@@ -8,16 +8,41 @@
 import SwiftUI
 
 struct LevelSelectorView: View {
+    
+    @State var isCompleted: [Bool] = LevelCompleted.isCompleted
+    
     var body: some View {
         NavigationStack {
             VStack(spacing: 50){
-                ForEach(Array(0..<LevelModel.fases().count), id: \.self){ level in
-                    NavigationLink(destination: LevelView(levelNumber: level, levelModel: LevelModel.fases())) {
-                        Text("Level \(level + 1)").font(.largeTitle)
-                    }
+                ForEach(Array(0..<LevelModel.patchOne().count), id: \.self){ level in
+                    NavigationLink(destination: LevelView(levelNumber: level, levelModel: LevelModel.patchOne())) {
+                        HStack{
+                            Text("Level \(level + 1)").font(.largeTitle)
+                            if isCompleted[level]{
+                                Text("✅")
+                            }
+                            
+                        }
+                    }.disabled(shouldDisable(level: level))
                 }
             }
+            .onAppear{
+                isCompleted = LevelCompleted.isCompleted
+            }
         }.navigationBarBackButtonHidden()
+    }
+    
+    func shouldDisable(level: Int) -> Bool{
+        if level == 0{
+            return false
+        }else if level == LevelModel.patchOne().count - 1{
+            if isCompleted[level - 1]{
+                return false
+            }
+            return true
+        }else{
+            return !isCompleted[level - 1]
+        }
     }
 }
 
