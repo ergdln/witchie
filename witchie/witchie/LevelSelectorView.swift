ce5bc15895250442927bd0000a563e18ccad5a8c
gms4 <gms4@cin.ufpe.br>
2023-07-26 23:24:24 -0300

    feature(implementingSound): sound on/off almost there, new StartGameView implemented and some levels added

--- witchie/witchie/LevelSelectorView.swift
+++ witchie/witchie/LevelSelectorView.swift
@@ -8,31 +8,13 @@
 import SwiftUI
 import AVFAudio
 
-//struct LevelSelectorView: View {
-//
-//    @State private var soundOn = true
-//    @StateObject private var audioPlayerManager = AudioPlayerManager()
-//
-//    var body: some View {
-//        NavigationStack {
-//            VStack(spacing: 50){
-//                ForEach(Array(0..<LevelModel.fases().count), id: \.self){ level in
-//                    NavigationLink(destination: LevelView(levelNumber: level, levelModel: LevelModel.fases())) {
-//                        Text("Level \(level + 1)").font(.largeTitle)
-//                    }
-//                }
-//                SoundToggle(soundOn: $soundOn, audioPlayerManager: audioPlayerManager)
-//            }
-//        }.navigationBarBackButtonHidden()
-//    }
-//}
 struct LevelSelectorView: View {
     @State private var soundOn = false
     @StateObject private var audioPlayerManager = AudioPlayerManager()
 
     var body: some View {
         NavigationStack {
-            VStack(spacing: 50) {
+            VStack(spacing: 25) {
                 ForEach(Array(0..<LevelModel.fases().count), id: \.self) { level in
                     NavigationLink(destination: LevelView(levelNumber: level, levelModel: LevelModel.fases())) {
                         Text("Level \(level + 1)").font(.largeTitle)
@@ -40,6 +22,7 @@ struct LevelSelectorView: View {
                 }
                 SoundToggle(soundOn: $soundOn, audioPlayerManager: audioPlayerManager)
             }
+            .frame(width: UIScreen.main.bounds.width * 0.8, height: UIScreen.main.bounds.width * 0.8)
         }
         .navigationBarBackButtonHidden()
         .onAppear {
