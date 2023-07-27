032356a2afcc005e29a168fa59ddbca8afdee7d1
ditthales <tvgf@academy.cin.ufpe.br>
2023-07-27 15:42:54 -0300

    adding-component-template

---
+++ witchie/witchie/UI/Components/ComponentX.swift
@@ -0,0 +1,20 @@
+//
+//  ComponentX.swift
+//  witchie
+//
+//  Created by ditthales on 27/07/23.
+//
+
+import SwiftUI
+
+struct ComponentX: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ComponentX_Previews: PreviewProvider {
+    static var previews: some View {
+        ComponentX()
+    }
+}
